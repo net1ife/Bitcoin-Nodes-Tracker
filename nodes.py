d4fc6de74cda36d8f59e3e6e81b31b516f1af2d6
net1ife <127271713+net1ife@users.noreply.github.com>
2023-07-04 18:22:44 -0500

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1,7 +1,6 @@
 import requests
 from collections import Counter
 
-
 def get_node_data():
     url = 'https://bitnodes.io/api/v1/snapshots/latest/'
     response = requests.get(url)
@@ -14,23 +13,23 @@ def get_node_data():
 
 
 def nodes_per_country(node_data):
-    # Country is the 8th item in the details.
-    countries = [details[7]
-                 for details in node_data.values() if len(details) > 7]
+    countries = [details[7] for details in node_data.values() if len(details) > 7]
     return Counter(countries)
 
 
 node_data = get_node_data()
 node_counts = nodes_per_country(node_data)
 
-# Sort by node count, in descending order
 sorted_node_counts = dict(
     sorted(node_counts.items(), key=lambda item: item[1], reverse=True))
 
 total_nodes = sum(sorted_node_counts.values())
 
-print(f"Total nodes: {total_nodes}\n")
-print(f"{'Country':<5} {'Nodes':<7}")
+print(f"Total number of nodes: {total_nodes}\n")
+print(f"{'Country':<20}{'Nodes':<10}")
+print("-" * 30)
+
 for country, count in sorted_node_counts.items():
     country = 'None' if country is None else country
-    print(f"{country:5} {count:<7}")
+    print(f"{country:<20}{count:<10}")
+
